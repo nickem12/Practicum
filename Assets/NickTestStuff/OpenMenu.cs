c982871c24cf410cbdf2480a4b065abd06c295ee
Nicholas Comeau <nick_comeau3@hotmail.com>
2017-05-05 11:35:04 -0400

    Added a song

--- Assets/NickTestStuff/OpenMenu.cs
+++ Assets/NickTestStuff/OpenMenu.cs
@@ -6,16 +6,29 @@ using UnityEngine.UI;
 
 public class OpenMenu : MonoBehaviour {
 
-    public GameObject panel;
-    public EventSystem eventSystem;
+    private Canvas can;
+    private EventSystem eventSystem;
+    private GameObject slotPrefab;
+    private GameObject inventory;
 
+    private void Start()
+    {
+        //can = GameObject.FindGameObjectWithTag("DropDown").GetComponent<Canvas>();
+        eventSystem = EventSystem.current;
+    }
     public void OpenDropDownMenu()
     {
-        panel.gameObject.SetActive(true);        
+        can = GameObject.FindGameObjectWithTag("DropDown").GetComponent<Canvas>();
+        eventSystem = EventSystem.current;
+        can.GetComponent<Canvas>().enabled = true;
+        eventSystem.SetSelectedGameObject(can.transform.GetChild(0).GetChild(0).gameObject);
     }
     public void CloseDropDownMenu()
     {
-        panel.gameObject.SetActive(false);
-        eventSystem.SetSelectedGameObject(this.gameObject);
+        can = GameObject.FindGameObjectWithTag("DropDown").GetComponent<Canvas>();
+        eventSystem = EventSystem.current;
+        inventory = GameObject.FindGameObjectWithTag("Inventory");
+        can.GetComponent<Canvas>().enabled = false;
+        eventSystem.SetSelectedGameObject(inventory.transform.GetChild(1).gameObject);
     }
 }
