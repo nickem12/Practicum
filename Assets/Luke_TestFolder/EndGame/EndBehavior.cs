25d0a71af4e969434e88cd170c8a9ff99d6f3960
LukeTrynchuk <luke123t@gmail.com>
2017-05-11 16:14:59 -0500

    #include <TheFeels2.0>

---
+++ Assets/Luke_TestFolder/EndGame/EndBehavior.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndBehavior : MonoBehaviour {
+
+	public bool isEnd;
+	public GameObject[] EndObjects;
+	public GameObject PlayerObject;
+
+	private Color colA;
+	private Color colB;
+	private float StartAlpha;
+	private float TotalDistance;
+	private float PlayerDistance;
+
+
+	void Start () {
+
+		colA = EndObjects[2].GetComponent<SkinnedMeshRenderer>().material.color;
+		colB = EndObjects[3].GetComponent<SkinnedMeshRenderer>().material.color;
+
+		StartAlpha = colA.a * 255;
+
+		TotalDistance = Vector3.Distance(this.transform.position, EndObjects[3].transform.position);
+	}
+	
+
+	void Update () {
+
+		if(isEnd){
+
+			PlayerDistance = Vector3.Distance(PlayerObject.transform.position, EndObjects[3].transform.position) - 5.0f;
+
+
+			if(PlayerDistance < TotalDistance){
+
+				float Tran = (PlayerDistance / TotalDistance) * StartAlpha / 255;
+				Debug.Log(Tran);
+
+				colA.a = Tran;
+				colB.a = Tran;
+				EndObjects[2].GetComponent<SkinnedMeshRenderer>().material.color = colA;
+				EndObjects[3].GetComponent<SkinnedMeshRenderer>().material.color = colB;
+
+			}
+			else{
+				colA.a = StartAlpha / 255;
+				colB.a = StartAlpha / 255;
+
+				EndObjects[2].GetComponent<SkinnedMeshRenderer>().material.color = colA;
+				EndObjects[3].GetComponent<SkinnedMeshRenderer>().material.color = colB;
+			}
+		}
+
+	}
+}
