11af59ee6199ef59e8bf8f3f173f82693213655b
Benjamin08 <RFLXcolby@gmail.com>
2017-05-16 12:01:43 -0300

    Adding a bunch of items
    Added some items to scenes, fixed some scene (debug reports). added
    collisions to pre apoc mall and player house.

--- Assets/Scripts/Misc/DataStorage.cs
+++ Assets/Scripts/Misc/DataStorage.cs
@@ -7,6 +7,20 @@ public class DataStorage : MonoBehaviour {
     private int slotIndex;
     public GameObject dropDownCanvas;
     public ItemData data;
+    private static bool Exists;
+
+    private void Start()
+    {
+        if (!Exists)
+        {
+            Exists = true;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
 
     public void SetSlotIndex(int value)
     {
