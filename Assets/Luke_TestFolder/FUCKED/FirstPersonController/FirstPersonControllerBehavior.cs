609c3488c9174a3bb64ab3a5807ced13d5f7415d
LukeTrynchuk <luke123t@gmail.com>
2017-05-16 16:41:58 -0500

    Finished FPS Controller

--- Assets/Luke_TestFolder/FUCKED/FirstPersonController/FirstPersonControllerBehavior.cs
+++ Assets/Luke_TestFolder/FUCKED/FirstPersonController/FirstPersonControllerBehavior.cs
@@ -1,12 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 public class FirstPersonControllerBehavior : MonoBehaviour {
 
-	
-	private Vector3 OldMouse;																			//The mouse position last frame
-	private Vector3 CurMouse;																			//The mouse position this frame
 	private Vector3 DiffMouse;																			//The difference in the mouse
 
 	private Vector3 ForwardVector;																		//The direction we would like to look in
@@ -16,20 +14,65 @@ public class FirstPersonControllerBehavior : MonoBehaviour {
 	public float SnapSensitivity;																		//The snapping sensitivity between the forward vector and the heading vector
 
 	public float HightRestraint;																		//What is the restraint to use for gimble lock?
+	private Camera MainCam;
+
+	private CharacterController CharController;															//The character controller component of the FPS controller
+	private Vector3 MoveDir;																			//The current move dir
+	private Vector3 ResultDir;																			//The result direction after all calculations
+
+	public float gravity;																				//The gravity of the object
+	public float jump;																					//The force of the jump
+	public float speed; 																				//The speed at which we move
+	private float DeltaHeight = 0.0f;																	//The change in height
+	private float DeltaGravity;
+	bool Groundedlast = false;
 
 	void Start () {
-		OldMouse = Input.mousePosition;																	//Use initial mouse position
 		ForwardVector = new Vector3(0,0,1);																//We start by looking forward in the z axis
 		HeadingVector = ForwardVector;																	//The way in which are actually looking
+		MainCam = Camera.main;																			//Get the main camera of the scene (Also a child to this object)
+		CharController = GetComponent<CharacterController>();											//Get the character controller component
+		DeltaGravity = gravity;
 	}
 
 
 	void Update () {
-
+		MoveDir = GetInput();																			//Get input from the player
 		DiffMouse = GetMouseInformation();																//Get the mouse information
 		RotateForwardVec();																				//Rotate the forward vector
 		RotateHeading();																				//Rotate the heading vector
 		RotateFPC();																					//Rotate the First Personal Controller
+		MoveController();																				//Move the controller
+	}
+
+	void FixedUpdate(){
+
+		Debug.DrawLine(this.transform.position, this.transform.position + (MoveDir * 5));
+		Debug.DrawLine(this.transform.position, this.transform.position + (HeadingVector * 2), Color.red);
+	}
+
+	private void MoveController(){
+		
+		ResultDir.x = MoveDir.x * speed;
+		ResultDir.z = MoveDir.z * speed;
+
+		if(Input.GetButton("Sprint")){
+			ResultDir.x *= 2;
+			ResultDir.z *= 2;
+		}
+
+		if(CharController.isGrounded){
+			DeltaGravity = gravity;
+			DeltaHeight = 0.0f;
+
+			if(Input.GetButtonDown("Jump")){
+				ResultDir.y = jump;
+			}
+		}
+
+		if(!CharController.isGrounded) DeltaGravity = DeltaGravity + DeltaGravity * (1.5f * Time.deltaTime);
+		ResultDir.y -= DeltaGravity * Time.deltaTime;
+		CharController.Move(ResultDir * Time.deltaTime);											//Move the character controller
 	}
 
 	private Vector3 GetMouseInformation(){
@@ -82,8 +125,8 @@ public class FirstPersonControllerBehavior : MonoBehaviour {
 	}
 
 	private void RotateFPC(){
-		transform.Rotate(-transform.rotation.eulerAngles);																										//Rotate the controller object to have 0 rotation on all axis
-		transform.LookAt(transform.position + HeadingVector);																									//Lookat the heading vector
+		MainCam.transform.Rotate(-MainCam.transform.rotation.eulerAngles);
+		MainCam.transform.LookAt(MainCam.transform.position + HeadingVector);
 	}
 
 	private void RotateHeading(){
@@ -97,6 +140,22 @@ public class FirstPersonControllerBehavior : MonoBehaviour {
 		else if(ForwardVector.y < -HightRestraint){
 			ForwardVector.y = -HightRestraint;
 		}
+	}
+
+	private Vector3 GetInput(){
+		Vector3 InputDir =  new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"), 0 , CrossPlatformInputManager.GetAxis("Vertical"));
+		if(InputDir.x == 0.0f && InputDir.z == 0.0f)
+			return Vector3.zero; 
+
+
+		float Angle = Vector3.Angle(Vector3.forward, new Vector3(HeadingVector.x, 0, HeadingVector.z));															//Get the angle between the forward of the world and our heading
+		float RightAngle = Vector3.Angle(Vector3.right, new Vector3(HeadingVector.x, 0, HeadingVector.z));														//The angle for the right to heading
+
+		if(RightAngle >= 90.0f) Angle *= -1.0f;
+
+		InputDir = Quaternion.Euler(0, Angle, 0) * InputDir;																									//Rotate the input dir by that angle on the y axis
+		InputDir.Normalize();																																	//Normalize the vector
+		return InputDir;																																		//Return the input direction
 	}
 
 }
