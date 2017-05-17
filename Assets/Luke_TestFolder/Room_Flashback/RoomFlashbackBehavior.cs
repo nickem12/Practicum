fa3e87f22791f410ae839980d19cf7fde133d25d
LukeTrynchuk <luke123t@gmail.com>
2017-05-17 16:45:01 -0500

    Finished the Room Flashback

--- Assets/Luke_TestFolder/Room_Flashback/RoomFlashbackBehavior.cs
+++ Assets/Luke_TestFolder/Room_Flashback/RoomFlashbackBehavior.cs
@@ -8,30 +8,69 @@ public class RoomFlashbackBehavior : MonoBehaviour {
 	public GameObject PostApoRoom;
 	public GameObject FlashBackRoom;
 	private bool Flashback;
+	private GameObject myPlayer;
+
+	private bool FirstTimeFlag = false;
+
 
 	void Start () {
 		PostApoRoom.SetActive(true);
 		FlashBackRoom.SetActive(false);
 		Flashback = false;
+		myPlayer = GameObject.FindGameObjectWithTag("Player");
 	}
 
 
 	public void Swap(){
-		if(Flashback){
-			Flashback = false;
-			PostApoRoom.SetActive(true);
-			FlashBackRoom.SetActive(false);
-		}
-		else{
-			Flashback = true;
-			PostApoRoom.SetActive(false);
-			FlashBackRoom.SetActive(true);
+		if(!FirstTimeFlag && Flashback || !Flashback){
+
+			if(Flashback){
+				PostApoRoom.SetActive(false);
+				FlashBackRoom.SetActive(true);
+			}
+			else{
+				PostApoRoom.SetActive(true);
+				FlashBackRoom.SetActive(false);
+			}
+
+			FirstTimeFlag = true;
 		}
 	}
 
 	void Update () {
-		if(Input.GetButtonDown("Sprint")){
-			Swap();
-		}
+		Debug.Log(Vector3.Distance(this.transform.position, myPlayer.transform.position));
 	}
+
+	void OnTriggerEnter(Collider Other){
+		if(Vector3.Distance(this.transform.position, Other.transform.position) <= 5.5f){
+			if (Other.gameObject.tag == "Player"){
+				Flashback = true;
+				Swap();
+			}
+		}
+		else{
+			if(Other.gameObject.tag == "Player"){
+				Flashback = true;
+				Swap();
+			}
+		}
+
+	}
+
+	void OnTriggerExit(Collider Other){
+		if(Other.gameObject.tag == "Player"){
+			if(Vector3.Distance(this.transform.position, Other.gameObject.transform.position) >= 5.5f){
+				Flashback = false;
+
+				Swap();
+			}
+			else{
+				Flashback = true;
+				Swap();
+			}
+		}
+
+	}
+
+
 }
