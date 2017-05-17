da880c9bc3851c9ed8655b538f4e922ed4d947ac
LukeTrynchuk <luke123t@gmail.com>
2017-05-17 10:20:07 -0500

    Tested Flashback Switching

---
+++ Assets/Luke_TestFolder/Room_Flashback/RoomFlashbackBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomFlashbackBehavior : MonoBehaviour {
+
+
+	public GameObject PostApoRoom;
+	public GameObject FlashBackRoom;
+	private bool Flashback;
+
+	void Start () {
+		PostApoRoom.SetActive(true);
+		FlashBackRoom.SetActive(false);
+		Flashback = false;
+	}
+
+
+	public void Swap(){
+		if(Flashback){
+			Flashback = false;
+			PostApoRoom.SetActive(true);
+			FlashBackRoom.SetActive(false);
+		}
+		else{
+			Flashback = true;
+			PostApoRoom.SetActive(false);
+			FlashBackRoom.SetActive(true);
+		}
+	}
+
+	void Update () {
+		if(Input.GetButtonDown("Sprint")){
+			Swap();
+		}
+	}
+}
