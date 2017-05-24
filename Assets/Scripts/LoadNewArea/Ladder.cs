ccb71ae57792593aeef5820c2b88e77fba0371cf
Benjamin08 <RFLXcolby@gmail.com>
2017-05-24 14:41:56 -0300

    Day and Night added
    Added day and night cycle to the actual outside scene. fixed grass and
    wall overlapping bug

---
+++ Assets/Scripts/LoadNewArea/Ladder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour {
+
+    public GameObject Dropoff;
+    private GameObject thePlayer;
+
+	// Use this for initialization
+	void Start () {
+        thePlayer = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
